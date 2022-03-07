867522bc873ec5274c453799169babf98fb81cc7
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-07 16:26:19 +0530

    stored images in over_lay list

--- virtual_painter.py
+++ virtual_painter.py
@@ -5,6 +5,11 @@ folder_path = 'Header Images'
 img_list = os.listdir(folder_path)
 img_list.sort()
 print(img_list)
+over_lay = []
+for img_path in img_list:
+    image = cv2.imread(f'{folder_path}/{img_path}')
+    over_lay.append(image)
+print(len(over_lay))
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1020)
